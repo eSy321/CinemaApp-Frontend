4c8e5c8e8de596f00609dd7bf262d430a4ae6131
Piotr <p.dybowski13@gmail.com>
2019-05-15 22:46:55 +0200

    testyyyy

--- src/index.js
+++ src/index.js
@@ -1,28 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom'
-import {BrowserRouter, Route} from 'react-router-dom'
-import Repertoire from './components/repertoire'
-import Kuba from './components/Kuba'
-
-const FirstPage = () => {
-    return <Repertoire />
-}
-
-const SeancePage = () => {
-    return <Kuba />
-}
+import Repertoire from './repertoire'
 
 const App = () => {
-    return (
-        <div>
-        <BrowserRouter>
-            <div>
-                <Route path="/" exact component={FirstPage}/>
-                <Route path="/Kuba" exact component={SeancePage}/>
-            </div>
-        </BrowserRouter>
-        </div>
-    )
+    return <Repertoire />;
 };
 
 ReactDom.render(
